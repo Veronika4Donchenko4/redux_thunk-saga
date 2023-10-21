bb2cab305a5701065bf80afdf3f6424070e59214
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-10-21 14:35:34 +0200

    use useState for button

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 const App = () => {
-  console.log('called');
   const [buttonText, setButtonText] = React.useState('Click me');
-
   const onButtonClick = () => {
-    setButtonText('Hello from React');
+    setButtonText('Click me pls');
   };
-
   return (
     <div className="app">
       <button onClick={onButtonClick}>{buttonText}</button>
     </div>
   );
 };
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
